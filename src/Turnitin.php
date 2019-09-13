c9b6eea7ac7939b79a9e237f64db02a3978eb275
Rizal Ardhi Rahmadani <rizal.ardhi.rahmadani@gmail.com>
2019-09-13 14:58:48 +0700

    environment

--- src/Turnitin.php
+++ src/Turnitin.php
@@ -4,7 +4,28 @@ namespace reyzeal;
 class Turnitin{
     private $session;
     protected $sessionPath;
-    public function __construct($username,$password,$forceToLogin=false,$sessionPath = __DIR__ . '/../../storage'){
+    public function __construct($username = null,$password = null,$forceToLogin = null,$sessionPath = null){
+        if(!$username){
+            if(function_exists('env')){
+                $username = env('TURNITIN_USERNAME');
+                $username = env('TURNITIN_PASSWORD');
+            }
+        }
+        if(!$forceToLogin){
+            if(function_exists('env')){
+                $forceToLogin = env('TURNITIN_FORCELOGIN');
+            }else{
+                $forceToLogin = false;
+            }
+        }
+        if(!$sessionPath){
+            if(function_exists('env')){
+                $sessionPath = env('TURNITIN_SESSION');
+            }
+            else{
+                $sessionPath = __DIR__ . '/../../storage';
+            }
+        }
         $this->sessionPath = $sessionPath;
         $this->session = new Turnitin\Session($username,$password,$sessionPath,$forceToLogin);
     }
